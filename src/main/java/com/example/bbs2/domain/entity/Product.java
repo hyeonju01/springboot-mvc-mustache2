376487c703cedd9208e29ce24a829fc0b852dac1
hyeonju01 <tangled6648@gmail.com>
2022-11-14 11:21:43 +0900

    feat: '스프링부트 핵심 가이드' 교재 실습 (107-108p.)

---
+++ src/main/java/com/example/bbs2/domain/entity/Product.java
@@ -0,0 +1,30 @@
+package com.example.bbs2.domain.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product") // 생략 가능
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number; //상품번호
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+
+
+
+
+}
